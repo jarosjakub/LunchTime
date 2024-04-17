420de70195cc36354f03bfb228091faafeb1180a
Jakub Jaroš <jarosj2@medtronic.com>
2024-04-17 09:44:28 +0200

    added icon

--- Program.cs
+++ Program.cs
@@ -35,13 +35,14 @@ namespace LunchTime
             var mlyn = new Mlyn();
             mlyn.GetMenu();
 
+
             var puor = new PUOR();
             puor.GetMenu();
 
             var kluci = new UKluku();
             kluci.GetMenu();
 
-            Console.Clear();
+            //Console.Clear();
 
             Console.WriteLine("PUOR" + "\n" + "-----------------------" + "\n");
             Console.WriteLine(puor.Menu);
